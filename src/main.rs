21c11e5fa3644be8f72ad41b0e38eb2e78efdae0
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-07-15 06:44:07 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use anyhow::Result;
 use log::{info, warn, error};
 use ark::RPC;
-
+use ark::VUTXO
 #[derive(Parser)]
 #[clap(name = "my_cli", about = "An example CLI application")]
 struct Cli {
